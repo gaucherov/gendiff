663851eac07c2865cbc6eb4ae6addb66a58a2eb0
Yuriy Shulga <jsshu1ga@gmail.com>
2024-03-28 01:30:19 +0300

    Add test of nested objects

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,8 +9,8 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 describe('plain', () => {
-  const diffpath = getFixturePath('expected_file.string');
-  const diff = fs.readFileSync(diffpath, 'utf8');
+  const diffPath = getFixturePath('plain_result.string');
+  const diff = fs.readFileSync(diffPath, 'utf8');
 
   test('plain json', () => {
     const filepath1 = getFixturePath('file1.json');
@@ -29,6 +29,18 @@ describe('plain', () => {
   test('plain yml', () => {
     const filepath1 = getFixturePath('file1.yml');
     const filepath2 = getFixturePath('file2.yml');
+
+    expect(genDiff(filepath1, filepath2)).toBe(diff);
+  });
+});
+
+describe('nested', () => {
+  const diffpath = getFixturePath('nested_result.string');
+  const diff = fs.readFileSync(diffpath, 'utf8');
+
+  test('nested json', () => {
+    const filepath1 = getFixturePath('nested1.json');
+    const filepath2 = getFixturePath('nested2.json');
 
     expect(genDiff(filepath1, filepath2)).toBe(diff);
   });
