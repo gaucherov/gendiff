e971f76d788d9c39d9f575d533f8088aab88651d
Yuriy Shulga <jsshu1ga@gmail.com>
2024-03-28 01:32:04 +0300

    Del plain tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -8,32 +8,6 @@ const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-describe('plain', () => {
-  const diffPath = getFixturePath('plain_result.string');
-  const diff = fs.readFileSync(diffPath, 'utf8');
-
-  test('plain json', () => {
-    const filepath1 = getFixturePath('file1.json');
-    const filepath2 = getFixturePath('file2.json');
-
-    expect(genDiff(filepath1, filepath2)).toBe(diff);
-  });
-
-  test('plain yaml', () => {
-    const filepath1 = getFixturePath('file1.yaml');
-    const filepath2 = getFixturePath('file2.yaml');
-
-    expect(genDiff(filepath1, filepath2)).toBe(diff);
-  });
-
-  test('plain yml', () => {
-    const filepath1 = getFixturePath('file1.yml');
-    const filepath2 = getFixturePath('file2.yml');
-
-    expect(genDiff(filepath1, filepath2)).toBe(diff);
-  });
-});
-
 describe('nested', () => {
   const diffpath = getFixturePath('nested_result.string');
   const diff = fs.readFileSync(diffpath, 'utf8');
