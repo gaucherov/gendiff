fe4046f92bc9e1f2f7471beaf101a6a44b51d254
Yuriy Shulga <jsshu1ga@gmail.com>
2024-03-29 23:14:32 +0300

    Feat support for json output

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,5 +1,6 @@
 import plain from './plain.js';
 import stylish from './stylish.js';
+import json from './json.js';
 
 const genFormatting = (diff, format = 'stylish') => {
   switch (format) {
@@ -8,6 +9,9 @@ const genFormatting = (diff, format = 'stylish') => {
 
     case 'plain':
       return plain(diff);
+
+    case 'json':
+      return json(diff);
 
     default:
       throw new Error('The unknown format!');
